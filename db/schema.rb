3b53b84a87b9a1997a8ddb2f2685eafbce8cc59c
Kipman <felippekipman@cjr.org.br>
2017-01-26 23:34:36 -0200

    -Atualizando banco com relacionamento entre usuarios e reembolsos

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125195304) do
+ActiveRecord::Schema.define(version: 20170126154320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20170125195304) do
     t.string   "value"
     t.string   "description"
     t.boolean  "status"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
+    t.integer  "user_id"
+    t.integer  "quantidade_moedas"
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +47,5 @@ ActiveRecord::Schema.define(version: 20170125195304) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
->>>>>>> upstream/master
+  add_foreign_key "refunds", "users"
 end
