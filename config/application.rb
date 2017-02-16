67d2e360b9c14cf303217847c2983d0b0dfdfcc7
TheKipmaster <felippekipman@cjr.org.br>
2017-02-16 14:45:31 -0200

    -Migration "AddAutorizadoToUsers" refeita

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,8 @@ module TestePT
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+
+    # Configuracao do arquivo local_env.yml
     config.before_configuration do
       env_file = File.join(Rails.root, 'config', 'local_env.yml')
       YAML.load(File.open(env_file)).each do |key, value|
