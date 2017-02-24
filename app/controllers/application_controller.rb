2a04b8815256cd7a61e3aff6c9aeebc55cc61f67
TheKipmaster <felippekipman@cjr.org.br>
2017-02-24 17:18:42 -0300

    -Modificada rota de redirecionamento apos sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   include PublicActivity::StoreController
-  
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   before_action :authenticate_user!
